7a2a6cb10aac92713f70228be9297638fc60da63
Vladimir Semenov <sepsseven@gmail.com>
2022-05-17 21:49:45 +0300

    add reservations

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+dev: build
+	echo "Starting dev server..."
+	docker-compose -f docker-compose.dev.yml up;
+
 build:
 	echo "Building..."
 	cd opservatory/client; npm run build;
--- opservatory/models.py
+++ opservatory/models.py
@@ -5,25 +5,39 @@ from enum import Enum
 from ipaddress import IPv4Address
 
 from pydantic import BaseModel
+from typing import Any, Optional
+from pydantic import BaseModel, SecretStr
+
+from opservatory.auth.models import User
 
 
-class OS(BaseModel):
+class Entity(BaseModel):
+    def update(self, other: Entity):
+        self.__dict__.update(self.__class__(**other.__dict__))
+        return self
+
+    def patch(self, data: dict[str, Any]):
+        self.__dict__.update(self.__class__(**(self.__dict__ | data)))
+        return self
+
+
+class OS(Entity):
     distribution: str
     version: str
 
 
-class Memory(BaseModel):
+class Memory(Entity):
     free: int
     total: int
 
 
-class Processor(BaseModel):
+class Processor(Entity):
     architecture: str  # ansible_architecture
     name: str  # ansible_processor[2]
     cores: int  # ansible_processor_cores
 
 
-class DockerContainer(BaseModel):
+class DockerContainer(Entity):
     tag: str
     name: str
     uptime: int
@@ -36,13 +50,28 @@ class MachineState(str, Enum):
     RESERVED = "reserved"
 
 
-class Machine(BaseModel):
+class Reservation(Entity):
+    user: User
+    reason: str
+
+    @staticmethod
+    def from_request(request: ReservationRequest, user: User) -> Reservation:
+        return Reservation(**request.dict(exclude={"machine_ip"}), user=user)
+
+
+class ReservationRequest(Entity):
+    reason: str
+    machine_ip: IPv4Address
+
+
+class Machine(Entity):
     ip: IPv4Address
     hostname: str
     ram: Memory  # ansible_memory_mb.real.total
     os: OS
     processor: Processor
     containers: list[DockerContainer]
+    reservation: Optional[Reservation] = None
     updated_at: datetime = datetime.now()
     state: MachineState = MachineState.UNREACHABLE
 
@@ -54,7 +83,7 @@ class Machine(BaseModel):
         self.updated_at = datetime.now()
 
 
-class Fleet(BaseModel):
+class Fleet(Entity):
     machines: list[Machine]
 
     @property
@@ -62,10 +91,15 @@ class Fleet(BaseModel):
         return {machine.ip: machine for machine in self.machines}
 
 
-class Config(BaseModel):
+class AuthConfig(Entity):
+    secret_key: SecretStr
+
+
+class Config(Entity):
     company_name: str
+    auth: AuthConfig
 
 
-class FrontendContext(BaseModel):
+class FrontendContext(Entity):
     machines: list[Machine]
     company_name: str
